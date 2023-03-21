b3cebd4699c6e6b27e1ae6f48203fe3ec2213097
Heattehnik <gadjka@mail.ru>
2023-03-21 21:06:46 +0500

    solved

--- tests/tests_arrs.py
+++ tests/tests_arrs.py
@@ -5,3 +5,10 @@ class Get(unittest.TestCase):
     def test_get(self):
         self.assertEqual(get([1, 2, 3], 2, "test"), 3)
         self.assertEqual(get([], 0, "test"), "test")
+
+    def test_my_slice(self):
+        self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(my_slice([], 0), [])
+        self.assertEqual(my_slice([1, 2, 3, 4], -2), [3, 4])
+        self.assertEqual(my_slice([1, 2, 3, 4], -5, -4), [])
